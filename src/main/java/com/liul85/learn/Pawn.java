30e786cb1a4e1ef863ae7dc148006517debc2e83
liul85 <liul85@gmail.com>
2016-03-02 22:48:42 +0800

    create first test for Pawn.

---
+++ src/main/java/com/liul85/learn/Pawn.java
@@ -0,0 +1,17 @@
+package com.liul85.learn;
+
+/**
+ * Created by twcn on 3/2/16.
+ */
+public class Pawn {
+
+    private String color;
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
---
+++ src/test/java/com/liul85/learn/PawnTest.java
@@ -0,0 +1,19 @@
+package com.liul85.learn;
+
+import com.liul85.learn.Pawn;
+
+/**
+ * Created by twcn on 3/2/16.
+ */
+public class PawnTest extends junit.framework.TestCase {
+    public void testPawnColor() throws Exception {
+        final String white = "white";
+        Pawn pawn = new Pawn(white);
+        assertEquals(white, pawn.getColor());
+
+
+        final String black = "black";
+        Pawn blackPawn = new Pawn(black);
+        assertEquals(black, blackPawn.getColor());
+    }
+}
